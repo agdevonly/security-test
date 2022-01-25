5547733cb4b770751b7e4573e2147fff72423e30
agdevonly <98386464+agdevonly@users.noreply.github.com>
2022-01-25 18:46:35 +0900

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,5 @@ var x, y, z;  // Declare 3 variables
 x = 5;    // Assign the value 5 to x
 y = 6;    // Assign the value 6 to y
 z = x + y;  // Assign the sum of x and y to z
+
+console.log(eval('2 + 2'));
